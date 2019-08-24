b8ecbbdb2b8ccfad19bae19dd5e72f6c1aec23c6
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-24 11:41:49 -0500

    dev

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node {
     stage("Pull Repo"){
-        properties([parameters([string(defaultValue: 'Dev', description: '''dev:  18.216.115.132
+        properties([parameters([string(defaultValue: 'dev', description: '''dev:  18.216.115.132
         prod: 18.223.109.202
         qa: 18.223.119.149''', name: 'Remote_instances', trim: false)])])
         git 'https://github.com/Nurjan87/jenkinsapril.git'
