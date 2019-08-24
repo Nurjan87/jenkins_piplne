5592b99e7fc66c8e632ee77c1845d86a4deaefab
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-24 11:29:09 -0500

    installin apache

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,11 @@
 node {
-    stage("Say Hello"){
+    stage("Pull Repo"){
         properties([parameters([string(defaultValue: 'Dev', description: '''dev:  18.216.115.132
         prod: 18.223.109.202
         qa: 18.223.119.149''', name: 'Remote_instances', trim: false)])])
         git 'https://github.com/Nurjan87/jenkinsapril.git'
+    }
+    stage("Install Apache"){
+        sh "sh ec2-user@${Remote_instances} sudo yum install httpd -y"
     }
 }
