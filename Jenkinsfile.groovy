881f26204c2b02da4ce21deadfca5f4d84e641da
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-24 11:24:34 -0500

    added dev qa prod

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,8 @@
 node {
     stage("Say Hello"){
-         git 'https://github.com/Nurjan87/jenkinsapril.git'
+        properties([parameters([string(defaultValue: 'Dev', description: '''dev:  18.216.115.132
+        prod: 18.223.109.202
+        qa: 18.223.119.149''', name: 'Remote_instances', trim: false)])])
+        git 'https://github.com/Nurjan87/jenkinsapril.git'
     }
 }
